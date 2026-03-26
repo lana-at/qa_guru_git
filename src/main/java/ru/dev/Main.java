1617c8f6aeb38183fe74bdb1ee1eb68246881091
lana-at <lanampr1986@gmail.com>
2026-03-26 09:48:50 +0500

    Update Main.java

--- src/main/java/ru/dev/Main.java
+++ src/main/java/ru/dev/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет! Изучаем конфликты!");
         System.out.println("Черт ногу сломит в этих ветках");
+        System.out.println("Подкиньте мозгов, кому не жалко");
     }
 }
