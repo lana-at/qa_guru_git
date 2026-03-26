3f8678c18e67e8704475600b754c75ff8a386d29
Neobutova.SI@parmalogica.ru <Neobutova.SI@parmalogica.ru>
2026-03-26 18:18:09 +0500

    rrr

--- src/main/java/ru/dev/Main.java
+++ src/main/java/ru/dev/Main.java
@@ -2,7 +2,7 @@ package ru.dev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет! Изучаем замечательные конфликты!");
+        System.out.println("Привет. Строка конфликта, надеюсь итоговая попытка....");
         System.out.println("Черт ногу сломит в этих ветках!!!!!!!");
         System.out.println("ваяю конфликт");
         System.out.println("Подкиньте мозгов, кому не жалко :)");
