035eeafa18cb4cadda8778887f29fc54cbbacf7f
Neobutova.SI@parmalogica.ru <Neobutova.SI@parmalogica.ru>
2026-03-26 12:20:17 +0500

    изменения

--- src/main/java/ru/dev/Main.java
+++ src/main/java/ru/dev/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Черт ногу сломит в этих ветках!!!!!!!");
         System.out.println("ваяю конфликт");
         System.out.println("Подкиньте мозгов, кому не жалко");
+        System.out.println("Памагити");
     }
 }
