de03580f189ef7465d7a771ba1df958eae29275a
Neobutova.SI@parmalogica.ru <Neobutova.SI@parmalogica.ru>
2026-03-26 10:01:37 +0500

    gjcktlybt bpvtytybz

--- src/main/java/ru/dev/Main.java
+++ src/main/java/ru/dev/Main.java
@@ -3,6 +3,8 @@ package ru.dev;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет! Изучаем конфликты!");
-        System.out.println("Черт ногу сломит в этих ветках");
+        System.out.println("Черт ногу сломит в этих ветках!!!!!!!");
+        System.out.println("ваяю конфликт");
+
     }
 }
